a03bf271ce13381415f0bbd3eec3cc45e5f9c099
Andressa Karoline <andressakdogado@gmail.com>
2020-09-30 21:32:07 -0300

    Atividade socket

--- client.py
+++ client.py
@@ -1,34 +1,28 @@
 import socket
 import time
 
-# Criar o socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-# Conectar ao servidor com ip e porta
 sock.connect(('localhost', 9000))
 
-# Leia o tamanho da mensagem
-expected_data_size = int(sock.recv(4).decode())
-print("Tamanho de dado esperado = {}".format(expected_data_size))
+while True:
+    expected_data_size = int(sock.recv(4).decode())
+    received_data = ''
+    
+    while len(received_data) < expected_data_size:
+        received_data += sock.recv(4).decode()
+    print("Servidor: " + received_data)
 
-received_data = ''
-while len(received_data) < expected_data_size:
-    # Ler o dado recebido
-    received_data += sock.recv(4).decode()
-    print("Tamanho do dado {}".format(len(received_data)))
-print(received_data)
-
-
-# Tamanho da mensagem
-mensagem = "Ola servidor"
-send_data_size = len(mensagem)
-sock.sendall(str(send_data_size).zfill(4).encode())
-
-# Enviar a mensagem
-sock.sendall(mensagem.encode())
-
-#
-time.sleep(3)
-
-# Finalizar a conexao
+    if received_data.lower() == "see ya":
+        print("Chat finalizado!")
+        break
+        
+    msg = raw_input("Cliente: ").strip()
+    send_data_size = len(msg)
+    sock.sendall(str(send_data_size).zfill(4).encode())
+    sock.sendall(msg.encode())
+    
+    if msg.lower() == "see ya":
+        print("Chat finalizado!")
+        break
+    
 sock.close()
